c6b020ab39b19fd0e63da0f4b20ed2f1e527b1a6
Laurent Redor <laurent.redor@obeo.fr>
2026-04-16 10:14:43 +0200

    Fix #40 – Switch MariaDB Docker tests to the native JDBC driver
    Update the Debian 12 + MariaDB test setup to use the native MariaDB JDBC
    driver instead of the MySQL one.
    
    This includes:
    - adding the MariaDB Java client to the bundled JDBC fragment
    - switching default MariaDB-oriented JDBC settings to
      org.mariadb.jdbc.Driver
    - replacing jdbc:mysql:// URLs with jdbc:mariadb:// URLs in test and
      application defaults
    - updating the MariaDB Docker Compose setup and documentation
    accordingly
    
    The full Docker test suite has been run successfully against Debian 12 +
    MariaDB with this native driver setup.

--- ui.web/plugins/org.activitymgr.ui.web.logic/src/org/activitymgr/ui/web/logic/LogicModule.java
+++ ui.web/plugins/org.activitymgr.ui.web.logic/src/org/activitymgr/ui/web/logic/LogicModule.java
@@ -139,8 +139,8 @@ public class LogicModule extends AbstractModule {
 	private DataSource createDataSource(IConfiguration cfg) {
 		BasicDataSource datasource = new BasicDataSource();
 		IConfiguration jdbcCfg = cfg.getScoped("activitymgr.jdbc", null);
-		datasource.setDriverClassName(jdbcCfg.get("driver", "com.mysql.cj.jdbc.Driver"));
-		datasource.setUrl(jdbcCfg.get("url", "jdbc:mysql://localhost:3306/taskmgr_db"));
+		datasource.setDriverClassName(jdbcCfg.get("driver", "org.mariadb.jdbc.Driver"));
+		datasource.setUrl(jdbcCfg.get("url", "jdbc:mariadb://localhost:3306/taskmgr_db"));
 		datasource.setUsername(jdbcCfg.get("user", "taskmgr"));
 		datasource.setPassword(jdbcCfg.get("password", "taskmgr"));
 		datasource.setDefaultAutoCommit(false);
